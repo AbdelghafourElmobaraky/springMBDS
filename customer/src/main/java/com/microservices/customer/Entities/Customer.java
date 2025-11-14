ae4b9a61a6ffa82bd856a912452b558add37bbc3
Abdelghafour <ozomaki925@gmail.com>
2025-11-14 15:10:57 +0100

    customer service 2

--- customer/src/main/java/com/microservices/customer/Entities/Customer.java
+++ customer/src/main/java/com/microservices/customer/Entities/Customer.java
@@ -1,4 +1,23 @@
 package com.microservices.customer.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
 public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
--- customer/src/main/java/com/microservices/customer/Repositories/CustomerRepository.java
+++ customer/src/main/java/com/microservices/customer/Repositories/CustomerRepository.java
@@ -1,4 +1,10 @@
 package com.microservices.customer.Repositories;
 
-public interface CustomerRepository {
+import com.microservices.customer.Entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
 }
