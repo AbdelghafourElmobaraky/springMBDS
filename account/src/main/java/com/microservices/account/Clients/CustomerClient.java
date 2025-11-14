ee7d06ff41bb413786b3e58f5d5e7ce6d5bb38c0
Abdelghafour <ozomaki925@gmail.com>
2025-11-14 16:39:45 +0100

    account service 1 + openfeign

---
+++ account/src/main/java/com/microservices/account/Clients/CustomerClient.java
@@ -0,0 +1,17 @@
+package com.microservices.account.Entities;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "customer-service", url = "http://localhost:8080")
+public interface CustomerClient {
+
+    @GetMapping("/customer/{id}")
+    Customer getCustomerById(@PathVariable Long id);
+
+    @GetMapping("/customers")
+    List<Customer> getAllCustomers();
+}
